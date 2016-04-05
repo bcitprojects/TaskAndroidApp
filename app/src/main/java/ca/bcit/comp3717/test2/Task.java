3f74ecb302c71b4d985c534bc1138f51795081ad
Ryan Stoppler <rstoppler01@gmail.com>
2016-04-04 23:53:37 -0700

    Task

---
+++ app/src/main/java/ca/bcit/comp3717/test2/Task.java
@@ -0,0 +1,36 @@
+package ca.bcit.comp3717.test2;
+
+/**
+ * Created by rbs on 2016-04-04.
+ */
+public class Task {
+    String title;
+    String description;
+    String priority;
+    public Task() {
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+}
